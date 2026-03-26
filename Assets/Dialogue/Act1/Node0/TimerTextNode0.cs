7996576dc7d9c6a8753932e425b7098c2d1c3225
[Diego Pena] <dipenaro@gmail.com>
2026-03-26 08:31:51 -0500

    Arreglada la escena antes del build

--- Assets/Dialogue/Act1/Node0/TimerTextNode0.cs
+++ Assets/Dialogue/Act1/Node0/TimerTextNode0.cs
@@ -42,8 +42,11 @@ public class TimerTextNode0 : MonoBehaviour
     public AudioSource DN0_3;
     public AudioSource Cinematic1;
     public AudioSource Music;
-    
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    [Header("Button Feedback")]
+    public AudioSource buttonAudioSource;
+    public AudioClip buttonClickSound;
+
     void Start()
     {
         /*videoPlayerNode0a.Stop();
@@ -52,16 +55,20 @@ public class TimerTextNode0 : MonoBehaviour
         videoPlayerNode0a.Play();*/
     }
 
-    // Update is called once per frame
     void Update()
     {
-        if(flagJustOneTime == false){
+        if (flagJustOneTime == false)
+        {
             StartCoroutine(AdvancingTimerNode0());
             flagJustOneTime = true;
         }
     }
 
-    public void ChangeNode0To1(){
+    public void ChangeNode0To1()
+    {
+        if (buttonAudioSource != null && buttonClickSound != null)
+            buttonAudioSource.PlayOneShot(buttonClickSound);
+
         GO_Node0.SetActive(false);
         GO_Node1.SetActive(true);
         timerNode1.StartTimerNode1();
@@ -70,7 +77,7 @@ public class TimerTextNode0 : MonoBehaviour
     IEnumerator AdvancingTimerNode0()
     {
         videoPlayerNode0a.Stop();
-        videoPlayerNode0a.time = 0;        
+        videoPlayerNode0a.time = 0;
         videoPlayerNode0a.frame = 0;
         videoPlayerNode0a.Play();
         Music.Play(); // Inicio Música de Fondo 
@@ -87,9 +94,9 @@ public class TimerTextNode0 : MonoBehaviour
 
         canvasFader.FadeAToB();
         videoPlayerNode0b.Stop();
-        videoPlayerNode0b.time = 0;        
+        videoPlayerNode0b.time = 0;
         videoPlayerNode0b.frame = 0;
-        
+
         Music.Stop(); // Fin música de Fondo Música de Fondo 
         videoPlayerNode0b.Play();
 
